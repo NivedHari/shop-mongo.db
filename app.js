534064441a26f11529d8602dcb69e88d008dc75f
NivedHari <nivedhari44@gmail.com>
2024-04-04 11:19:39 +0530

    mongoose implemented

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 require("dotenv").config();
+const mongoose = require("mongoose");
 
 const errorController = require("./controllers/error");
-const mongoConnect = require("./util/database").mongoConnect;
-const User = require("./models/user");
+
+// const User = require("./models/user");
 const app = express();
 
 app.set("view engine", "ejs");
@@ -17,19 +18,26 @@ const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
-app.use((req, res, next) => {
-  User.findById("660bbbbf39bbd1b2b7068d5f").then((user) => {
-    req.user = new User(user.name, user.email, user.cart, user._id);
-    next();
-  });
-});
+// app.use((req, res, next) => {
+//   User.findById("660bbbbf39bbd1b2b7068d5f").then((user) => {
+//     req.user = new User(user.name, user.email, user.cart, user._id);
+//     next();
+//   });
+// });
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(() => {
-  console.log("Connected");
-  app.listen(3000);
-});
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@shop.belb8l9.mongodb.net/${process.env.MONGO_COLLECTION}?retryWrites=true&w=majority&appName=shop`
+  )
+  .then((result) => {
+    app.listen(3000);
+    console.log("connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
